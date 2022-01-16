b007cfd79925ac69e9322e82116cc85c2e74de45
pablobenper <80679646+pablobenper@users.noreply.github.com>
2022-01-16 18:29:16 +0000

    Add files via upload

---
+++ proyectoJenkins/src/main/java/pruebaUnitaria.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author depfult
+ */
+
+public class pruebaUnitaria {
+
+    public class MiClase {
+    public static boolean dia_laboral(String dia) {
+        switch(dia) {
+         case "Lunes":
+           return true;
+         case "Martes":
+            return true;
+         case "Miercoles":
+            return true;
+         case "Jueves":
+            return true;
+         case "Viernes":
+            return true;
+        case "Sabado":
+            return false;
+        case "Domingo":
+            return false;
+        default:
+            return false;
+}
+}
+}
+}
